1dad87d38b3e411bc029b653be9e726841ad648b
gmgrahamgm <grahamd@tamu.edu>
2024-09-06 17:46:35 -0500

    Fixed index.html css locating bug in both java and main html page

--- scripts/script.js
+++ scripts/script.js
@@ -11,8 +11,17 @@ function handleClick(checkbox) {
 
 function changeStyle(style) {
   const currentPage = localStorage.getItem('currentPage');
-  document.getElementById("page-style").setAttribute("href", `../css/${style}_${currentPage}.css`);
-  document.getElementById("nav-style").setAttribute("href", `../css/${style}_navbar.css`);
+  let accessor = "";
+  console.log("Current page = [" + currentPage + "]");
+
+  if (currentPage === "index") {
+    accessor = ".";
+  } else {
+    accessor = "..";
+  }
+
+  document.getElementById("page-style").setAttribute("href", `${accessor}/css/${style}_${currentPage}.css`);
+  document.getElementById("nav-style").setAttribute("href", `${accessor}/css/${style}_navbar.css`);
 
   if (style === 'style1') {
     document.getElementById("font-style").setAttribute("href", "https://fonts.googleapis.com/css?family=Lato");
@@ -20,8 +29,8 @@ function changeStyle(style) {
     document.getElementById("font-style").setAttribute("href", "https://fonts.googleapis.com/css?family=Open+Sans:400,600,300");
   }
 
-  console.log("Changed page-style to ", `../css/${style}_${currentPage}.css`);
-  console.log("Changed nav-style to ", `../css/${style}_navbar.css`, "\n")
+  console.log("Changed page-style to ", `${accessor}/css/${style}_${currentPage}.css`);
+  console.log("Changed nav-style to ", `${accessor}/css/${style}_navbar.css`, "\n")
 }
 
 // On page reload, check stored style checkbox state and assign it to the checkbox that exists on the current page.
